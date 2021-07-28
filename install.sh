4eae841bbd2f86816ac05981e25556497b614c35
Gijs Entius <g.m.entius@gmail.com>
2021-07-28 18:09:35 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,4 @@ wget https://github.com/jgm/pandoc/releases/download/2.14.1/pandoc-2.14.1-1-amd6
 sudo dpkg -i pandoc-2.14.1-1-amd64.deb
 
 wget https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.12.0b/pandoc-crossref-Linux.tar.xz
+tar -xf pandoc-crossref-Linux.tar.xz
