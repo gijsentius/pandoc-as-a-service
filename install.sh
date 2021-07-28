aa6d09613703e19dec43ea01828d86d41be31244
Gijs Entius <g.m.entius@gmail.com>
2021-07-28 18:03:01 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 sudo apt install texlive-full
+sudo apt install openjdk-11-jre
 
 wget https://github.com/jgm/pandoc/releases/download/2.14.1/pandoc-2.14.1-1-amd64.deb
 sudo dpkg -i pandoc-2.14.1-1-amd64.deb
 
 wget https://github.com/lierdakil/pandoc-crossref/releases/download/v0.3.12.0b/pandoc-crossref-Linux.tar.xz
-
